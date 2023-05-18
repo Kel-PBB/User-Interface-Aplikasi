24b4e91609b799a8412f2d5460a364a6498f4147
Kopriyanto <kopriyan003@gmail.com>
2023-05-19 04:06:57 +0700

    edit navbar

---
+++ app/src/main/java/com/example/cloudrent/HomeActivity.kt
@@ -0,0 +1,43 @@
+package com.example.cloudrent
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.cloudrent.databinding.ActivityHomeBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class HomeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityHomeBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(HomeFragment())
+
+        binding.bottonnav.setOnItemSelectedListener {
+
+            when(it.itemId){
+                R.id.navigation_home -> replaceFragment(HomeFragment())
+                R.id.navigation_loc -> replaceFragment(LocationFragment())
+                R.id.navigation_notifications -> replaceFragment(NotificcationsFragment())
+                R.id.navigation_notifications -> replaceFragment(ProfileFragment())
+
+                else -> {
+
+                }
+            }
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.nav_host, fragment)
+        fragmentTransaction.commit()
+    }
+}
--- app/src/main/java/com/example/cloudrent/Login.kt
+++ app/src/main/java/com/example/cloudrent/Login.kt
@@ -3,6 +3,7 @@ package com.example.cloudrent
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
@@ -12,6 +13,12 @@ class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val btnLogin: Button = findViewById(R.id.btnLet)
+        btnLogin.setOnClickListener {
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+        }
     }
 
     fun onTextClicked(view: View) {
