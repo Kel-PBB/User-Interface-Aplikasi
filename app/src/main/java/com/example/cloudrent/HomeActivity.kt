655ee447d6abf0829ce1fb025b7fde470f07a8e5
Firmansyah <firman326syah@gmail.com>
2023-05-19 21:01:01 +0700

    UPDATE | 19 - 05 -2023

--- app/src/main/java/com/example/cloudrent/HomeActivity.kt
+++ app/src/main/java/com/example/cloudrent/HomeActivity.kt
@@ -3,15 +3,11 @@ package com.example.cloudrent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
 import com.example.cloudrent.databinding.ActivityHomeBinding
-import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class HomeActivity : AppCompatActivity() {
     private lateinit var binding: ActivityHomeBinding
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
